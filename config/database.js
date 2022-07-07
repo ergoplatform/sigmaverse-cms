ad71723caa9cbfb513eba7fc450887aadab1dff5
deadit <stdeadit@gmail.com>
2022-07-07 17:39:20 +0300

    update docekr

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ module.exports = ({ env }) => ({
     client: 'postgres',
     connection: {
       host: env('DATABASE_HOST', 'postgres-cms'),
-      port: env.int('DATABASE_PORT', 5432),
+      port: env.int('DATABASE_PORT', 5434),
       database: env('DATABASE_NAME', 'postgres'),
       user: env('DATABASE_USERNAME', 'postgres'),
       password: env('POSTGRES_PASSWORD'),
